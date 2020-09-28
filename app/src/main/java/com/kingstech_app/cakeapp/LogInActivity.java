4d2c37828c33cd90cfe4daf751d71ec2def936e3
ghostkh2020 <kumlachew12292000@gmail.com>
2020-09-28 10:40:55 -0400

    Worked on saving user data on user phone database and using that info when loging in

--- app/src/main/java/com/kingstech_app/cakeapp/LogInActivity.java
+++ app/src/main/java/com/kingstech_app/cakeapp/LogInActivity.java
@@ -2,11 +2,16 @@ package com.kingstech_app.cakeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 public class LogInActivity extends AppCompatActivity {
     Button logIn, signUp;
@@ -30,19 +35,25 @@ public class LogInActivity extends AppCompatActivity {
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences mPrefs = getSharedPreferences("Name", Context.MODE_PRIVATE);
+                Gson gson = new Gson();
+                String json = mPrefs.getString("Key", "");
+                User userObj = gson.fromJson(json, User.class);
                 if(nameET.getText().toString().equals("Admin")&& passwordET.getText().toString().equals("123"))
                 {
                     Intent intent = new Intent(LogInActivity.this,CakeOrderList.class);
                     startActivity(intent);
                 }
-                else
+                else if(nameET.getText().toString().equals(userObj.getName()) && passwordET.getText().toString().equals(userObj.getPassword()))
                 {
                     Intent intent = new Intent(LogInActivity.this, SettingActivity.class);
                     startActivity(intent);
                 }
-
-
-
+                else
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),R.string.loginERROR,Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
--- app/src/main/java/com/kingstech_app/cakeapp/SignUpActivity.java
+++ app/src/main/java/com/kingstech_app/cakeapp/SignUpActivity.java
@@ -2,13 +2,20 @@ package com.kingstech_app.cakeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
+//import com.google.gson.Gson;
 
 public class SignUpActivity extends AppCompatActivity {
     Button signup, login;
+    EditText nameET,passwordET, phoneET,emailET,adressET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +23,59 @@ public class SignUpActivity extends AppCompatActivity {
 
         signup = (Button) findViewById(R.id.signupBTN);
         login = (Button) findViewById(R.id.loginBTN);
+        nameET = (EditText) findViewById(R.id.snameET);
+        passwordET = (EditText) findViewById(R.id.spasswordET);
+        phoneET = (EditText) findViewById(R.id.sphoneET);
+        emailET = (EditText) findViewById(R.id.semailET);
+        adressET = (EditText) findViewById(R.id.saddressET);
+        if(check())
+        {
+            Intent intent = new Intent(SignUpActivity.this,LogInActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else
+        {
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(SignUpActivity.this,LogInActivity.class);
+                    startActivity(intent);
+                }
+            });
+            signup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    User user;
+                    user = new User(nameET.getText().toString(),passwordET.getText().toString(),phoneET.getText().toString(),emailET.getText().toString(),adressET.getText().toString());
+                    SharedPreferences mPrefs = getSharedPreferences("Name", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor prefsEditor = mPrefs.edit();
+                    Gson gson = new Gson();
+                    String json = gson.toJson(user);
+                    prefsEditor.putString("key",json);
+                    prefsEditor.commit();
+                }
+            });
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignUpActivity.this,LogInActivity.class);
-                startActivity(intent);
-            }
-        });
+        }
+
+
+
+    }
+    public boolean check()
+    {
+        SharedPreferences mPrefs = getSharedPreferences("Name", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("Key", "");
+        User userObj = gson.fromJson(json, User.class);
+        if(json.equals(""))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
 }
